d165cb278034f32fb959cd2d510056cd3c8ddfdf
TranDinhTin <tdt.smile1@gmail.com>
2023-06-24 23:03:38 +0700

    build frontend

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -10,7 +10,7 @@ import { TransformInterceptor } from './modules/common/interceptors/TransformInt
 import * as express from 'express';
 import { ErrorFilter } from './modules/errors/error.filter';
 
-async function bootstrap() { build false
+async function bootstrap() {
   const logger = new AppLogger();
   logger.info(`NodeJs Version ${process.version}`);
   logger.info(JSON.stringify(process.env));
